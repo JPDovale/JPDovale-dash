5cd90770cecbf9267417db629685f47eeb0f5b48
João Paulo do Vale <100259992+JPDovale@users.noreply.github.com>
2023-11-23 18:21:36 -0300

    adding pointer on card project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,12 @@
 import { Component } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { RouterOutlet } from '@angular/router'
-import { FallingLettersComponent } from '../components/falling-letters/falling-letters.component'
 import { HeaderComponent } from '../components/header/header.component'
-import { ProjectsComponent } from '../components/projects/projects.component'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    CommonModule,
-    RouterOutlet,
-    FallingLettersComponent,
-    HeaderComponent,
-    ProjectsComponent,
-  ],
-
+  imports: [CommonModule, RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
