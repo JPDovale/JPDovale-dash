668ce0a45bf86d7a7869c2bd461906a9ee91c6f4
João Paulo do Vale <100259992+JPDovale@users.noreply.github.com>
2023-11-25 04:16:57 -0300

    Adding about me on portifólio

--- src/pages/home/home.component.ts
+++ src/pages/home/home.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { FallingLettersComponent } from '../../components/falling-letters/falling-letters.component'
 import { HeaderComponent } from '../../components/header/header.component'
 import { ProjectsComponent } from '../../components/projects/projects.component'
+import { client } from '../../config/prismicConfig'
+import { asHTML } from '@prismicio/client'
 
 @Component({
   selector: 'app-home',
@@ -16,4 +18,13 @@ import { ProjectsComponent } from '../../components/projects/projects.component'
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit {
+  aboutMe = ''
+
+  async ngOnInit(): Promise<void> {
+    const { results } = await client.getByType('about_me')
+    const { content } = results[0]?.data
+
+    this.aboutMe = asHTML(content) ?? ''
+  }
+}
